23297539b2ec4e1621a6baa6e71bd6fbc101f8cf
mbaisotti99 <matteo.baisotti@gmail.com>
2024-11-20 11:17:16 +0100

    feat:milestone 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 const cardSpace = document.querySelector("#post-place");
 let postArray = [];
+const btnRegen = document.querySelector("#regen");
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
 
@@ -14,7 +15,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
             <div class="col-12 col-md-6 col-lg-4 text-center">
                         <div class="post-card mt-5">
                             <img src="./img/pin.svg" alt="pin" id="pin">
-                            <img src=${curPost.thumbnailUrl} alt="asdnojkl" class="m-5">
+                            <img src=${curPost.thumbnailUrl} alt="asdnojkl">
                             <p class="p-5">
                                 ${curPost.title}
                             </p>
@@ -24,6 +25,30 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
     });
 
 });
+
+btnRegen.addEventListener("click", () => {
+    cardSpace.innerHTML = ""
+    postArray = []
+
+    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp =>{
+        postArray = resp.data;
+
+        postArray.forEach((curPost) =>{
+            cardSpace.innerHTML += 
+            `
+                <div class="col-12 col-md-6 col-lg-4 text-center">
+                            <div class="post-card mt-5">
+                                <img src="./img/pin.svg" alt="pin" id="pin">
+                                <img src=${curPost.thumbnailUrl} alt="asdnojkl">
+                                <p class="p-5">
+                                    ${curPost.title}
+                                </p>
+                            </div>
+                        </div>
+            `
+        });
+    })
+})
 
 
 
