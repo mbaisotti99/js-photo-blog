be56f49b0a5ea94bf220b88b568dc7164ca1159f
mbaisotti99 <matteo.baisotti@gmail.com>
2024-11-20 12:04:12 +0100

    ultima milestone prebonus

--- js/script.js
+++ js/script.js
@@ -3,19 +3,28 @@ const cardSpace = document.querySelector("#post-place");
 let postArray = [];
 const btnRegen = document.querySelector("#regen");
 
+
+
+const overlayElem = document.querySelector(".overlay");
+
+const xElem = document.querySelector(".x");
+
+
+
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
 
-    
+
     postArray = resp.data;
     console.log(postArray);
 
-    postArray.forEach((curPost) =>{
-        cardSpace.innerHTML += 
-        `
+    postArray.forEach((curPost) => {
+
+        cardSpace.innerHTML +=
+            `
             <div class="col-12 col-md-6 col-lg-4 text-center">
                         <div class="post-card mt-5">
                             <img src="./img/pin.svg" alt="pin" id="pin">
-                            <img src=${curPost.thumbnailUrl} alt="asdnojkl">
+                            <img src=${curPost.thumbnailUrl} alt="asdnojkl" class="post-img" data-img-id="${curPost.id}">
                             <p class="p-5">
                                 ${curPost.title}
                             </p>
@@ -24,22 +33,40 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
         `
     });
 
+    const postImgElem = document.querySelectorAll(".post-img");
+    postImgElem.forEach((curImg) =>{
+        
+        curImg.addEventListener("click", () => {
+            overlayElem.classList.remove("d-none")
+            overlayElem.classList.add("d-block")
+
+            // console.log(curImg.dataset.imgId);
+            // curImgId = curImg.dataset.imgId;
+            const modalImgElem = document.querySelector(".modalImg")
+            modalImgElem.src = curImg.src;
+            // console.log("click");
+            
+            // overlayElem.style.display = "block";
+        })
+
+    })
+
 });
 
 btnRegen.addEventListener("click", () => {
     cardSpace.innerHTML = ""
     postArray = []
 
-    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp =>{
+    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
         postArray = resp.data;
 
-        postArray.forEach((curPost) =>{
-            cardSpace.innerHTML += 
-            `
+        postArray.forEach((curPost) => {
+            cardSpace.innerHTML +=
+                `
                 <div class="col-12 col-md-6 col-lg-4 text-center">
                             <div class="post-card mt-5">
                                 <img src="./img/pin.svg" alt="pin" id="pin">
-                                <img src=${curPost.thumbnailUrl} alt="asdnojkl">
+                                <img src=${curPost.thumbnailUrl} alt="asdnojkl" class="post-img">
                                 <p class="p-5">
                                     ${curPost.title}
                                 </p>
@@ -47,9 +74,17 @@ btnRegen.addEventListener("click", () => {
                         </div>
             `
         });
+
     })
 })
 
+xElem.addEventListener("click", () => {
+    overlayElem.classList.remove("d-block")
+    overlayElem.classList.add("d-none")
+})
 
-
+overlayElem.addEventListener("click", () =>{
+    overlayElem.classList.remove("d-block")
+    overlayElem.classList.add("d-none")
+})
 
